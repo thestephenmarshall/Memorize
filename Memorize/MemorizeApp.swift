e5fae1f8d4446fc92a6565699730416bcd148010
Stephen Marshall <stephen.marshall@powerhrg.com>
2023-04-18 14:31:16 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Stephen Marshall on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
